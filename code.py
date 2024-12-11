958cf0482539f1563c08ac6895e2448dbd09d62d
NimoK16 <nimish.kapoor.cd.eee23@itbhu.ac.in>
2024-12-12 04:46:04 +0530

    Length specified

--- code.py
+++ code.py
@@ -13,14 +13,15 @@ def generate_password(length, use_lowercase, use_uppercase=True, use_digits=True
     return p
 def main():
     print("Welcome to the Password Generator!")
-    length = int(input("Enter the desired password length: "))
-    use_lowercase = input("Include lowercase letters? (y/n): ").lower() == 'y'
+    length = int(input("Enter the desired password length between 6 to 30: "))
     use_uppercase = input("Include uppercase letters? (y/n): ").lower() == 'y'
     use_digits = input("Include numbers? (y/n): ").lower() == 'y'
     use_symbols = input("Include special characters? (y/n): ").lower() == 'y'
+    if length<6 and length>30:
+        raise ValueError(f"Password length out of range")
     try :
            
-        password = generate_password(length,use_lowercase, use_uppercase, use_digits, use_symbols)
+        password = generate_password(length, use_uppercase, use_digits, use_symbols)
         print(f"Your generated password is: {password}")
     except ValueError as error:
         
